4e3276b82710e62af35e50031399d469bfc88b2e
Q. Price <priceqp@gmail.com>
2020-06-06 16:12:35 -0500

    complete steps one and two

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from surveys import surveys
 
 app = Flask(__name__)
+responses = []
 
 
+# Handle root route - render survey
 @app.route("/")
-def test():
-    return ("Testing routes!")
+def start_survey():
+    survey = surveys["satisfaction"]
+    return render_template("home.html", survey=survey)
